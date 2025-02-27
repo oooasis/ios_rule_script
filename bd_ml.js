2030775efcfdf4a3f95084d125dcbd218e44e67f
oooasis <39504742+oooasis@users.noreply.github.com>
2025-02-27 21:18:44 +0800

    Update and rename bd_mll.js to bd_ml.js

--- bd_ml.js
+++ bd_ml.js
@@ -21,14 +21,12 @@ function tunnelTLSFinished() {
     _writeHttpHeader()
     httpStatus = HTTP_STATUS_CONNECTED
     return true
-console.log("Tls握手成功")
 }
 //从代理服务器读取到数据回调
 function tunnelDidRead(data) {
     if (httpStatus == HTTP_STATUS_WAITRESPONSE) {
         //check http response code == 200
         //Assume success here
-        console.log("http handshake success")
         httpStatus = HTTP_STATUS_FORWARDING
         $tunnel.established($session)//
         return null//
@@ -40,13 +38,11 @@ function tunnelDidRead(data) {
 function tunnelDidWrite() {
     if (httpStatus == HTTP_STATUS_CONNECTED) {
         console.log("write http head success")
- console.log("写请求头成功")
         httpStatus = HTTP_STATUS_WAITRESPONSE
         $tunnel.readTo($session, "\x0D\x0A\x0D\x0A")//读取远端数据直到出现\r\n\r\n
         return false //中断wirte callback
     }
     return true
-console.log("发送至代理服务器成功")
 }
 //会话关闭
 function tunnelDidClose() {
@@ -58,7 +54,6 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nConnection: keep-alive\r\n\r\n`;
-console.log("执行完成释放内存")
+    var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nConnection: keep-alive\r\n\r\n`
     $tunnel.write($session, header)
 }
